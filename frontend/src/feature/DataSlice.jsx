7554db4dbd9623d49cbb25671e4f9fd90682d860
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-10-08 15:49:34 +0530

    Remove unused code

--- frontend/src/feature/DataSlice.jsx
+++ frontend/src/feature/DataSlice.jsx
@@ -1,23 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-/*
-export const fetchCategories = createAsyncThunk('E-COMMERCE/FETCH_CATEGORIES', async () => {
-  const response = await axios.get('https://dummyjson.com/products/categories');
-  return response.data;
-});
-
-export const searchProducts = createAsyncThunk('E-COMMERCE/SEARCH_PRODUCTS', async (search) => {
-  const response = await axios.get(`https://dummyjson.com/products/search?q=${search}`);
-  return sortData(response.data.products);
-});
-
-export const searchCategories = createAsyncThunk('E-COMMERCE/PRODUCTS_BY_CATEGORY', async (value) => {
-  const response = await axios.get(`https://dummyjson.com/products/category/${value}`);
-  return(sortData(response.data.products));
-});
-*/
-
 export const fetchList = createAsyncThunk('FETCH_LIST', async (value) => {
 	const response = await axios.get('http://localhost:8000/api/data')
 	return response.data;
